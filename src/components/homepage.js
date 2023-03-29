7f40050db794b9e7fd9cbe97d8ea9d6d78eebb53
Roshan <rxgody@gmail.com>
2023-03-29 23:01:15 +0530

    done

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -6,6 +6,7 @@ function Homepage() {
     const { Configuration, OpenAIApi } = require("openai");
 const [res,setRes]=useState("");
 const [loading,setLoading]=useState(false);
+
 const configuration = new Configuration({
   apiKey: "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",
 });
@@ -13,14 +14,14 @@ const openai = new OpenAIApi(configuration);
 async function hello(){
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `Write a recipe based on these ingredients ${data} and Instructions:`,
+        prompt: `Write a recipe based on these ingredients ${data} and give only Instructions:`,
         temperature: 0.3,
         max_tokens: 200,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
       });
-      console.log(response);
+      console.log(response);  
       setRes(response.data.choices[0].text);
       
 }
@@ -40,10 +41,27 @@ const handleClick=async()=>{
     <div className='head'>
       <h1 className='heading'>Let's get to cooKING!!</h1>
       <img className='head-img' alt="food" src='https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=867&q=80'/>
+      <a href='#page1'><button className="headbtn"><span class="material-symbols-outlined">
+arrow_drop_down_circle
+</span></button></a>
     </div>
-    <div>
-      <h1>LETS COOK</h1>
-      <input></input>
+    <div className='second-page'>
+      <h1 id='page1'>What ingredients have you got?</h1>
+      <div className='sidebyside1'>
+        <div className='ing'>
+
+        <textarea placeholder='Eg: 1)Egg' className='ingredients' onChange={HandleChange}></textarea>
+        <a href='#page3'><button className='ing-btn' onClick={handleClick}>RECIPE</button></a>
+        </div>
+     
+      <img src='https://images.unsplash.com/photo-1503453776591-b4548af666a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNoZWYlMjBhbmltYXRlZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'  alt="chef" className='chefimg'/>
+      </div>
+    </div>
+    <div id="page3">
+      <div>Here's your recipe</div>
+      <div>
+        <textarea className='recipe' value={res}>{res}</textarea>
+      </div>
     </div>
    </div>
   )
